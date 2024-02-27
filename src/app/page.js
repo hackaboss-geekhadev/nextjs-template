08a34b0660af208b0d3fb5fce67dd07e3d4466eb
Irwing Naranjo <khalisser@gmail.com>
2024-02-27 07:54:03 -0300

    Update .gitignore file

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,6 @@
+// Esta es una contraseña de un servicio de pago no debo compartirla
+const API_KEY_PAYMENT = 'ABCDEF12345'
+
 export default function Home () {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
