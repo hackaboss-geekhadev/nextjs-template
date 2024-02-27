e6005f5cc88005738ebad419293930b8dd238153
Irwing Naranjo <khalisser@gmail.com>
2024-02-27 08:24:52 -0300

    Add Title component and test

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,9 @@
-// Esta es una contraseña de un servicio de pago no debo compartirla
-const API_KEY_PAYMENT = 'ABCDEF12345'
+import Title from '@/components/Title'
 
 export default function Home () {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Template Nextjs</h1>
+      <Title title='home' />
     </main>
   )
 }
