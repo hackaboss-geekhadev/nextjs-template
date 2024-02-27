8abcde0d4f3d3bb321ce5b6a200976b0f51addc5
Irwing Naranjo <khalisser@gmail.com>
2024-02-27 08:45:16 -0300

    Add test files and configurations for end-to-end testing

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,8 @@ export default function Home () {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Title title='home' />
+
+      <a href="/about" className="text-blue-500 hover:underline">about</a>
     </main>
   )
 }
